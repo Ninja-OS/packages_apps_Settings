5e666d473a8309bf9fdd3987f6902d04e916b5ec
iamsj7 <shaikjaleel270@gmail.com>
2018-04-11 18:31:09 +0000

    Move to Ninja

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -193,6 +193,6 @@ public class Settings extends SettingsActivity {
     public static class SupportDashboardActivity extends SettingsActivity {}
     public static class NinjaShadowHubActivity extends SettingsActivity {}
 
-    // Abc Additions.
+    // NINJA Additions.
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
